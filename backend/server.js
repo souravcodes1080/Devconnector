397aa2d1f72d8609e45eb2a0d4504fce12edef5d
souravcodes1080 <souravch2003@gmail.com>
2025-07-24 22:12:05 +0530

    initial route setup

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ app.get("/", (req, res)=>{
     res.send("API working...")
 })
 
+//define routes
+app.use('/api/users', require('./routes/api/user'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/post'))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
