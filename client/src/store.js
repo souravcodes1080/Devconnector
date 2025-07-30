b81c52c29dd5210ecbb36b2574f96e3cae00dba9
souravcodes1080 <souravch2003@gmail.com>
2025-07-30 13:19:21 +0530

    onrender setup

--- client/src/store.js
+++ client/src/store.js
@@ -1,39 +1,39 @@
 // npm install redux-devtools-extension --legacy-peer-deps
 //testing server ->
 
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import  {thunk}  from "redux-thunk";
-import rootReducer from "./reducers";
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import  {thunk}  from "redux-thunk";
+// import rootReducer from "./reducers";
 
-const initialState = {};
+// const initialState = {};
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
 
-export default store;
+// export default store;
 
 
 
 // npm uninstall redux-devtools-extension --legacy-peer-deps
 //live server ->
 
-// import { createStore, applyMiddleware } from "redux";
-// import { thunk } from "redux-thunk"; // 👈 fixed
-// import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk"; // 👈 fixed
+import rootReducer from "./reducers";
 
-// const initialState = {};
-// const middleware = [thunk];
+const initialState = {};
+const middleware = [thunk];
 
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   applyMiddleware(...middleware)
-// );
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
 
-// export default store;
+export default store;
